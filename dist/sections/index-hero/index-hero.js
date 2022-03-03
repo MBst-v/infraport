c54faf0062244e3382edc692629ba4028e837755
MediaBay <med.bay@yandex.ru>
2022-03-03 12:23:03 +0300

    fixes

--- dist/sections/index-hero/index-hero.js
+++
@@ -1,7 +0,0 @@
-// ;(function() {
-//   let headerHeight = hdr.offsetHeight,
-//     heroPicture = q('.index-hero__pic');
-
-//   heroPicture.style.cssText = 'top:-' + headerHeight + 'px;height:calc(100% + ' + headerHeight + 'px)';
-
-// })();
--- dist/template-parts/index-about.php
+++
@@ -1,9 +0,0 @@
-<section class="index-about container sect-bg lazy" data-src="#"<?php echo $sect_id ?>>
-  <div class="sect-left">
-    <h2 class="index-about__sect-title sect-title-5"><span class="sect-title-text"><?php echo $section['sect_title'] ?></span></h2>  
-  </div>
-  <div class="sect-right">
-    <span class="index-about__title sect-title-2"><?php echo $section['title'] ?></span>
-    <p class="index-about__descr"><?php echo $section['descr'] ?></p>
-  </div>
-</section>
