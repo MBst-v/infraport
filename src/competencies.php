6ff5df097c1d1224fa0aac19f2d100f27d86b458
vdm <exgart.mbf@gmail.com>
2022-03-21 11:08:24 +0300

    fix

--- src/competencies.php
+++ src/competencies.php
@@ -9,8 +9,8 @@ get_header();
 foreach ( $GLOBALS['sections'] as $section ) {
 	if ( $section['is_visible'] ) {
 		$section_id = $section['sect_id'] ? ' id="' . $section['sect_id'] . '"' : '';
+		require 'template-parts/' . $section['acf_fc_layout'] . '.php';
 	}
-	require 'template-parts/' . $section['acf_fc_layout'] . '.php';
 }
 
 get_footer();
